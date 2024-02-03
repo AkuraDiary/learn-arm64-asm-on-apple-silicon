ed6610653443a166024d77833795dc9b75171ee9
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-02-03 08:28:44 +0700

    first commit

---
+++ hellow.s
@@ -0,0 +1,31 @@
+//
+// hellow world
+//
+
+.global _start
+.align 2
+
+_start:
+  adr x1, message	// address the string message into x1 register
+  b _print		// call the print function
+  
+  b _terminate
+
+_print:
+  // Parameters
+  // x1 : the string to be printed
+  // x2 : the len of the string
+
+  mov X0, #1 		// stdout  
+  ldr x2, =message_len	// address the initial len into the x2 register
+  mov X16, #4		// write to stdout
+  svc 0			// syscall
+
+_terminate:
+  mov X0, #0 	// return 0
+  mov X16, #1	// terminate
+  svc 0		// syscal
+
+
+message: .asciz "hellow world \n"
+message_len= .-message 
