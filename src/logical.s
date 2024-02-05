d11a5c38a2390a3f30096b19d66ba2f2ed064739
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2024-02-05 14:59:04 +0700

    the and, or eor and negation operator

--- src/logical.s
+++ src/logical.s
@@ -7,14 +7,23 @@
 
 
 _start:
-  mov X1, #0x42
-  mov X2, #0x26
+  mov X1, #0x42 // #66
+  mov X2, #0x26 // #38
 
-  and X0, X1, X2 	// now for some reason, the and operator can't use a register and direct value
+  // this should output 2 (in the exit code) in dec
+  // and X0, X1, X2 	// now for some reason, the and operator can't use a register and direct value
 			// i.e and X0, X1, #0x26 ( it didnt work) and you have to put the value
 			// into another register first
 
-  
+  // this should output 102 (in the exit code) in dec
+  // orr X0, X1, X2  
+
+  // this should output 100 (in the exit code) in dec
+  // eor X0, X1, X2
+
+  // this is negation operator, move and negate the B (X1) value into A (X0)
+  // this should output 189 in dec (in the exit code)
+  mvn X0, X1
 
   b _terminate
 
